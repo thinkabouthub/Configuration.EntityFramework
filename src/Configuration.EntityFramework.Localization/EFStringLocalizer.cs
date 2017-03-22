c578f17fb3c8d136bd147350ed3e0ffc2d3e1802
Patrick <patrick.nolan@thinkabout.com.au>
2017-03-22 21:36:53 +1000

    Adding Configuration.EntityFramework.Localization back

---
+++ src/Configuration.EntityFramework.Localization/EFStringLocalizer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using Microsoft.Extensions.Localization;
+
+namespace Configuration.EntityFramework.Localization
+{
+    public class EFStringLocalizer : IStringLocalizer
+    {
+        private readonly ConfigurationContext Context;
+        private readonly string CultureName;
+        private const string AspectName = "Localization";
+
+        public EFStringLocalizer(ConfigurationContext context) : this(context, CultureInfo.CurrentUICulture) { }
+
+        public EFStringLocalizer(ConfigurationContext context, CultureInfo cultureInfo)
+        {
+            this.Context = context;
+            this.CultureName = cultureInfo.Name;
+        }
+
+        public virtual LocalizedString this[string name]
+        {
+            get
+            {
+                var value = this.GetString(name);
+                return new LocalizedString(name, value ?? name, resourceNotFound: value == null);
+            }
+        }
+
+        public virtual LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var format = GetString(name);
+                var value = string.Format(format ?? name, arguments);
+                return new LocalizedString(name, value, resourceNotFound: format == null);
+            }
+        }
+
+        protected virtual string GetString(string name)
+        {
+            var section = this.Context.Sections.FirstOrDefault(s => s.Aspect == AspectName && (string.IsNullOrEmpty(this.CultureName) || s.SectionName == this.CultureName));
+            if (section != null)
+            {
+                var setting = this.Context.Settings.FirstOrDefault(s => s.SectionId == section.Id && s.Key == name);
+                return setting?.GetValue<string>();
+            }
+            return name;
+        }
+
+        public virtual IStringLocalizer WithCulture(CultureInfo culture)
+        {
+            return new EFStringLocalizer(this.Context, culture);
+        }
+
+        public virtual IEnumerable<LocalizedString> GetAllStrings(bool includeAncestorCultures)
+        {
+            var section = this.Context.Sections.FirstOrDefault(s => s.Aspect == AspectName && (string.IsNullOrEmpty(this.CultureName) || s.SectionName == this.CultureName));
+            if (section != null)
+            {
+                return this.Context.Settings.Select(s => new LocalizedString(s.Key, s.GetValue<string>(), true));
+            }
+            return new Collection<LocalizedString>();
+        }
+    }
+}
---
+++ src/Configuration.EntityFramework.Localization/EFStringLocalizerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Localization;
+
+namespace Configuration.EntityFramework.Localization
+{
+    public class EFStringLocalizerFactory : IStringLocalizerFactory
+    {
+        private readonly ConfigurationContext Context;
+
+        public EFStringLocalizerFactory(ConfigurationContext context)
+        {
+            this.Context = context;
+        }
+
+        public IStringLocalizer Create(Type resourceSource)
+        {
+            return new EFStringLocalizer(this.Context);
+        }
+
+        public IStringLocalizer Create(string baseName, string location)
+        {
+            return new EFStringLocalizer(this.Context);
+        }
+    }
+}
